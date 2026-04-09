6369c7c766875f75eb07a1dc02ad4bf23d0379db
SHIVAM SINGH <shivamsingh281021@gmail.com>
2026-04-09 12:42:26 +0530

    Modify test_record.py with new credentials
    Updated email and password fields in test_record.py.

--- New folder/playwright demo 1/tests/test_record.py
+++ New folder/playwright demo 1/tests/test_record.py
@@ -5,9 +5,9 @@ from playwright.sync_api import Page, expect
 # def test_example(page: Page) -> None:
 #     page.goto("https://war.onerooftechnologiesllp.com/")
 #     page.get_by_role("textbox", name="name@example.com").click()
-#     page.get_by_role("textbox", name="name@example.com").fill("shivam.singh@onerooftech.com")
+#     page.get_by_role("textbox", name="name@example.com").fill("gmail.com")
 #     page.get_by_role("textbox", name="Password").click()
-#     page.get_by_role("textbox", name="Password").fill("Shivam@1234")
+#     page.get_by_role("textbox", name="Password").fill("password")
 #     expect(page.get_by_role("textbox", name="name@example.com")).to_be_visible()
 #     expect(page.get_by_role("textbox", name="Password")).to_be_visible()
 #     page.get_by_role("button", name="Login").click()
